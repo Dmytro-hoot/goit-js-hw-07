8af6b8371c7070af4ed2ce62e07a2d72d5756ae5
Dmytro <46649352+Dmytro-hoot@users.noreply.github.com>
2023-07-27 17:11:35 +0300

    Start 1st gallery mrkup

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,18 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryBox = document.querySelector('.gallery');
+const galleryCards = createCards(galleryItems);
+
+function createCards(galleryItems) {
+	return galleryItems.map(({ original, preview, description }) => {
+		return `<div class="gallery__item">
+  		 	<a class="gallery__link" href="${original}">
+      		 <img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}"/>
+         </a>
+      </div>`;
+	})
+	 .join('');
+}
+	
+galleryBox.insertAdjacentHTML('beforeend', galleryCards);
