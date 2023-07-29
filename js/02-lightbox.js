3065f47703f677b528762dd9cad486a0d6ec49e1
Dmytro <46649352+Dmytro-hoot@users.noreply.github.com>
2023-07-29 21:22:00 +0300

    2 завдання +

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,45 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+// Створення розмітки галереї
+
+// Створюємо елементи
+
+const galleryBox = document.querySelector('.gallery');
+const galleryCards = createCards(galleryItems);
+
+// Створюємо розмітку
+
+function createCards(galleryItems) {
+	return galleryItems
+		.map(({ original, preview, description }) => {
+		return `<li class="gallery__item">
+  		 	<a class="gallery__link" href="${original}">
+      		 <img class="gallery__image" src="${preview}" alt="${description}"/>
+         </a>
+      </li>`;
+	})
+	 .join('');
+}
+
+// Приклад нової розмітки
+
+// <li class="gallery__item">
+//    <a class="gallery__link" href="large-image.jpg">
+//       <img class="gallery__image" src="small-image.jpg" alt="Image description" />
+//    </a>
+// </li>
+
+// Додавання розмітки в список
+
+galleryBox.insertAdjacentHTML('beforeend', galleryCards);
+
+// Додавання модалки
+
+
+const lightbox = new SimpleLightbox('.gallery a', { 
+	
+	captionsData: 'alt',
+	captionDelay: 250
+
+});
